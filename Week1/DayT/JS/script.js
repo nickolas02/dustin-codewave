8ef300220ff92db79b5ded517e257d4c27e0756f
Nickolas <deckernickolas@gmail.com>
2021-07-29 14:31:36 -0700

    added joke app to day3

---
+++ Week1/DayT/JS/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("click", function(event) {
+    if (!event.target.matches("#button")) 
+    return;
+
+    fetch("https://official-joke-api.appspot.com/random_joke")
+    .then((response) => response.json())
+    .then((data) => renderJoke(data)) 
+    .catch(() => renderError());
+
+});
+
+function renderJoke(data) {
+    const setup = document.getElementById("setup");
+    const punchline = document.getElementById("punchline");
+    const error = document.getElementById("error");
+    
+    error.innerHTML == "";
+    setup.innerHTML = data.setup;
+    punchline.innerHTML = data.punchline;
+}
+
+function renderError() {
+    const error = document.getElementById("error");
+    error.innerHTML = "whoops, something broke. please try again";
+
+}
